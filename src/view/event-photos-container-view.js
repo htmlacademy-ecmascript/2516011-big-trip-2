f34c8e5657d36835a6379d1ff1a4ef07f658320b
bertruchio <mr.2003.artem@gmail.com>
2024-09-11 00:56:39 +0300

    Разобьёт код на модули

---
+++ src/view/event-photos-container-view.js
@@ -0,0 +1,31 @@
+import { createElement } from '../render.js';
+
+function createEventPhotosContainerTemplate() {
+  return (`<div class="event__photos-container">
+                      <div class="event__photos-tape">
+                        <img class="event__photo" src="img/photos/1.jpg" alt="Event photo">
+                        <img class="event__photo" src="img/photos/2.jpg" alt="Event photo">
+                        <img class="event__photo" src="img/photos/3.jpg" alt="Event photo">
+                        <img class="event__photo" src="img/photos/4.jpg" alt="Event photo">
+                        <img class="event__photo" src="img/photos/5.jpg" alt="Event photo">
+                      </div>
+                    </div>`);
+}
+
+export default class NewEventPhotosContainerView {
+  getTemplate() {
+    return createEventPhotosContainerTemplate();
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
