a17e2dd012ce3a1845ff1397fd718ca416c8e845
bertruchio <mr.2003.artem@gmail.com>
2024-09-30 00:31:53 +0300

    Добавит моковые данные

---
+++ src/mock/destinations.js
@@ -0,0 +1,46 @@
+export const mockDestinations = [
+  {
+    id: 'bfa5cb75-a1fe-4b77-a83c-0e528e910e04',
+    name: 'Chamonix',
+    description: 'Chamonix, is a beautiful city, a true Asian pearl, with crowded streets.',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.0762563005163317',
+        description: 'Chamonix parliament building'
+      }
+    ]
+  },
+  {
+    id: 'd1c24c1e-a8cf-11eb-bcbc-0242ac130002',
+    name: 'Geneva',
+    description: 'Geneva, a calm and peaceful city near the Alps.',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.527836451',
+        description: 'Geneva lake view'
+      }
+    ]
+  },
+  {
+    id: 'e8c3b27f-a8cf-11eb-bcbc-0242ac130002',
+    name: 'Zurich',
+    description: 'Zurich, a major city in Switzerland, known for its financial institutions.',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.827173821',
+        description: 'Zurich skyline'
+      }
+    ]
+  },
+  {
+    id: 'f9b5db61-b1fe-4b77-a83c-0e528e910e04',
+    name: 'Paris',
+    description: 'Paris, the city of lights and romance, known for its culture and fashion.',
+    pictures: [
+      {
+        src: 'http://picsum.photos/300/200?r=0.174873920',
+        description: 'Eiffel Tower view'
+      }
+    ]
+  }
+];
---
+++ src/mock/offers.js
@@ -0,0 +1,62 @@
+export const mockOffers = [
+  {
+    type: 'taxi',
+    offers: [
+      {
+        id: 'b4c3e4e6-9053-42ce-b747-e281314baa31',
+        title: 'Upgrade to a business class',
+        price: 120
+      },
+      {
+        id: 'e3f4e556-89c3-41bb-bae2-45b2d58d5512',
+        title: 'Choose a premium taxi',
+        price: 200
+      }
+    ]
+  },
+  {
+    type: 'bus',
+    offers: [
+      {
+        id: 'e6c2e5a8-8af4-11eb-8dcd-0242ac130003',
+        title: 'Extra baggage',
+        price: 50
+      },
+      {
+        id: 'h8e2e1f9-8af4-11eb-8dcd-0242ac130006',
+        title: 'Seat reservation',
+        price: 30
+      }
+    ]
+  },
+  {
+    type: 'train',
+    offers: [
+      {
+        id: 'f7c3a3a8-8af4-11eb-8dcd-0242ac130004',
+        title: 'First class seat',
+        price: 150
+      },
+      {
+        id: 'i8d3f1f9-8af4-11eb-8dcd-0242ac130007',
+        title: 'Meal included',
+        price: 70
+      }
+    ]
+  },
+  {
+    type: 'flight',
+    offers: [
+      {
+        id: 'h9c8e8a8-8af4-11eb-8dcd-0242ac130005',
+        title: 'Extra legroom',
+        price: 100
+      },
+      {
+        id: 'j9e8e1f9-8af4-11eb-8dcd-0242ac130008',
+        title: 'Priority boarding',
+        price: 80
+      }
+    ]
+  }
+];
