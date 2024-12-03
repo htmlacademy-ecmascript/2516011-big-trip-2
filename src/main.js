5c1ce2811aca043b1fd3d4a29c5f1e714efc1e85
bertruchio <mr.2003.artem@gmail.com>
2024-12-03 14:31:28 +0300

    Добавит модель для фильтра. WIP Задача модели - хранить выбранный фильтр и уведомлять наблюдателей, если таковой изменится.
    WIP: линтер ругается на неиспользуемые переменные

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import BoardPresenter from './presenter/board-presenter.js';
 import PointsModel from '../src/model/points-model.js';
+import FilterModel from './model/filter-model.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   const siteTripEventsElement = document.querySelector('.trip-events');
   const pointsModel = new PointsModel();
+  const filterModel = new FilterModel();
   pointsModel.init();
   const boardPresenter = new BoardPresenter({container: siteTripEventsElement, pointsModel: pointsModel});
 
