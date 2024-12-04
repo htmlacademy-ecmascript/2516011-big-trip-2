0fc7edfd90b30923d883657e17bea1a0efb31e64
bertruchio <mr.2003.artem@gmail.com>
2024-12-05 00:51:51 +0300

    fix: возвращает обратно "отключение" фильтра, если к нему нет точек маршрута

--- src/view/trip-filter-view.js
+++ src/view/trip-filter-view.js
@@ -1,7 +1,7 @@
 import AbstractView from '../framework/view/abstract-view.js';
 
 const createTripFilterItemTemplate = (filter, currentFilterType) => {
-  const { type } = filter;
+  const { type, count } = filter;
   return (
     `<div class="trip-filters__filter">
       <input
@@ -11,6 +11,7 @@ const createTripFilterItemTemplate = (filter, currentFilterType) => {
         name="trip-filter"
         value="${type}"
         ${type === currentFilterType ? 'checked' : ''}
+        ${count === 0 ? 'disabled' : ''}
       />
       <label class="trip-filters__filter-label" for="filter-${type}">
         ${type}
